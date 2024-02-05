c013e48ade9abb33581bbaae000c9ab45e140dd0
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-02-05 18:03:18 +0300

    Add Functionality to Get Data From the API

--- EuropeanCountries/Views/HomeVC.swift
+++ EuropeanCountries/Views/HomeVC.swift
@@ -10,49 +10,66 @@ import UIKit
 class HomeVC: UIViewController {
     
     let tableView = UITableView()
-    let countries: [Country] = []
+    var countries: [Country] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        title = "Countries"
+        
         configureTableView()
-        
-        NetworkManager.shared.getCountries { countries, errorMessage in
-            guard let countries = countries else {
-                print("olmadi be knk")
-                return
-            }
-            print(countries)
-        }
-        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getCountryList()
     }
     
     func configureTableView() {
         view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.frame = view.bounds
-        tableView.rowHeight = 80
         tableView.delegate = self
         tableView.dataSource = self
         
+
+        tableView.frame = view.bounds
         
-        tableView.register(SavedCell.self, forCellReuseIdentifier: SavedCell.reuseID)
+        tableView.register(HomeCell.self, forCellReuseIdentifier: HomeCell.reuseID)
     }
+    
+    
+    func getCountryList() {
+        NetworkManager.shared.getCountries { countries, errorMessage in
+            guard let countries = countries else {
+                print("olmadi be knk")
+                return
+            }
+            self.countries = countries
+            //print(self.countries)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.view.bringSubviewToFront(self.tableView)
+            }
+        }
+    }
+    
+    
 }
 
 extension HomeVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        countries.count
+        //print(countries.count)
+        return countries.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: SavedCell.reuseID) as! SavedCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: HomeCell.reuseID) as! HomeCell
         let country = countries[indexPath.row]
         cell.set(country: country)
         
         return cell
     }
-
+    
+    
     
 }
