f6393e747d344d8cf72b8cafdd71c3ac71ae9eef
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-02-05 18:00:51 +0300

    Add Star Image

--- EuropeanCountries/Utilities/Constants.swift
+++ EuropeanCountries/Utilities/Constants.swift
@@ -10,4 +10,5 @@ import UIKit
 struct Constants {
     static let home = UIImage(systemName: "house.fill")
     static let heart = UIImage(systemName: "heart.fill")
+    static let star = UIImage(systemName: "star.fill")
 }
