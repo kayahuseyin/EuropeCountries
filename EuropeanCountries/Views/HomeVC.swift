08d597a9669b66859e639218783c0c8fc62f229e
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-02-06 16:51:18 +0300

    Add spacing between cells

--- EuropeanCountries/Views/HomeVC.swift
+++ EuropeanCountries/Views/HomeVC.swift
@@ -11,17 +11,23 @@ class HomeVC: UIViewController {
     
     let tableView = UITableView()
     var countries: [Country] = []
+    let attributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18.0)] // make title bold
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        title = "Countries"
-        
+        configureViewController()
         configureTableView()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         getCountryList()
+    }
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.titleTextAttributes = attributes
+        title = "Countries"
     }
     
     func configureTableView() {
@@ -30,17 +36,21 @@ class HomeVC: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
-
-        tableView.frame = view.bounds
-        
         tableView.register(HomeCell.self, forCellReuseIdentifier: HomeCell.reuseID)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -20),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
     }
     
     
     func getCountryList() {
         NetworkManager.shared.getCountries { countries, errorMessage in
             guard let countries = countries else {
-                print("olmadi be knk")
+                print("Error fetching countries")
                 return
             }
             self.countries = countries
@@ -58,18 +68,33 @@ class HomeVC: UIViewController {
 extension HomeVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //print(countries.count)
-        return countries.count
+        return countries.count * 2 - 1 //in order to prevent adding one more emptyCell after the last cell do - 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: HomeCell.reuseID) as! HomeCell
-        let country = countries[indexPath.row]
-        cell.set(country: country)
         
-        return cell
+        if indexPath.row % 2 == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: HomeCell.reuseID) as! HomeCell
+            let countryIndex = indexPath.row / 2
+            let country = countries[countryIndex]
+            cell.set(country: country)
+            
+            return cell
+        } else {
+            let emptyCell = UITableViewCell()
+            emptyCell.backgroundColor = .systemBackground
+            emptyCell.selectionStyle = .none
+            
+            return emptyCell
+        }
     }
     
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        if indexPath.row % 2 == 0 {
+            return 40
+        }
+        return 15
+    }
     
 }
