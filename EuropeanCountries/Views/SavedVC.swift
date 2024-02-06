80df979e71dc01e53faf6382cc6b0f6b3edb5803
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-02-06 17:05:02 +0300

    Configure ViewController

--- EuropeanCountries/Views/SavedVC.swift
+++ EuropeanCountries/Views/SavedVC.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class SavedVC: UIViewController {
+    
+    let attributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18.0)]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureViewController()
+    }
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.titleTextAttributes = attributes
+        title = "Countries"
     }
 }
