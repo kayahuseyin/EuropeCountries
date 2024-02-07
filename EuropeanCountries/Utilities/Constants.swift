00341f4ed39e468c1cb61d33a5e986f9ead913b4
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-02-07 15:16:32 +0300

    Add arrow constant

--- EuropeanCountries/Utilities/Constants.swift
+++ EuropeanCountries/Utilities/Constants.swift
@@ -11,4 +11,5 @@ struct Constants {
     static let home = UIImage(systemName: "house.fill")
     static let heart = UIImage(systemName: "heart.fill")
     static let star = UIImage(systemName: "star.fill")
+    static let arrow = UIImage(systemName: "arrow.backward")
 }
