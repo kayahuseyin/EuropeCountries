53ca3f6a009e1e4aebe124ec2a81142c1ba6e149
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-02-08 14:02:31 +0300

    Add getCountryDetails

--- EuropeanCountries/Views/CountryDetailVC.swift
+++ EuropeanCountries/Views/CountryDetailVC.swift
@@ -10,10 +10,33 @@ import UIKit
 class CountryDetailVC: UIViewController {
 
     var country : Country?
+    var countryDetails: [CountryDetail] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureViewController()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getCountryDetails()
+    }
+    
+    
+    func getCountryDetails() {
+        NetworkManager.shared.getCountryDetails { [weak self] details, errorMessage in
+            guard let self = self else { return }
+            guard let details = details else {
+                print("error fetching details")
+                return
+            }
+            self.countryDetails = details
+            print(self.countryDetails)
+        }
+    }
+    
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
         title = country?.name.common
     }
